86fd6d7b50380193c4d78079fd173ff2296b2a97
Ruddah Bagga <ruddahbagga@hotmail.com>
2019-09-24 20:20:42 -0400

    Rigged game logic, needs grid system

---
+++ FourInARow/Assets/Scripts/BoardLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BoardLogic : MonoBehaviour
+{
+    private int[,] board;
+    private int columns, rows, vertical, horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vertical = (int)Camera.main.orthographicSize;
+        horizontal = vertical * (Screen.width / Screen.height);
+        rows = 5; columns = 6;
+        board = new int[6, 7];
+
+        for (int r = 0; r >= rows; r++)
+        {
+            for (int c = 0; c >= columns; c++)
+            {
+                board[r, c] = -1;
+            }
+        }
+
+
+    }
+
+    private void SpawnSlot(int x, int y)
+    {
+        GameObject g = new GameObject("X: " + x + " Y: " + y);
+    }
+
+    void ClearBoard()
+    {
+        
+    }
+}
